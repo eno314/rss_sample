a033afa3b918888bd1cc6debc68f41adfa15a470
Hiroto Kitamura <kirikonknow@gmail.com>
2018-02-09 23:24:46 +0900

    commit

--- app/src/main/kotlin/jp/eno314/rssreader/MainActivity.kt
+++ app/src/main/kotlin/jp/eno314/rssreader/MainActivity.kt
@@ -2,12 +2,46 @@ package jp.eno314.rssreader
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import jp.eno314.rssreader.rss.RssRepository
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.android.UI
 
 
 class MainActivity : AppCompatActivity() {
 
+    private var job: Job? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        job = launch(UI) {
+            try {
+                val rss = async(CommonPool) {
+                    RssRepository().request("https://dev.classmethod.jp/feed/")
+                }.await()
+                rss?.let {
+                    Log.d("BBBBB", "title : " + it.channel.title)
+                    it.channel.itemList.forEach { item ->
+                        Log.d("BBBBB", "item title : " + item.title)
+                    }
+                }
+            } catch (e: CancellationException) {
+                e.printStackTrace()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        job?.cancel()
     }
 }
